ea12571cf5e9472131d30f15efef150320d58f4d
Abhijeet anil Rathod <nueonboy@gmail.com>
2024-02-04 01:01:44 +0530

    Update 1.1 2024

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
       <header id="home" className="flex flex-col-reverse md:flex-row w-full h-screen max-w-7xl items-center justify-center p-8 relative overflow-x-hidden">
         <div className='w-full h-2/4 md:h-full md:w-2/5 flex flex-col justify-center items-center md:items-start gap-8'>
           <div className='flex flex-col gap-2'>
-            <h1 className='text-4xl font-black md:text-8xl'>Insightful</h1>
+            <h1 className='text-4xl font-black md:text-8xl'>KODAKEY</h1>
             <h2 className='text-md md:text-2xl'>Start growing today!</h2>
           </div>
           <p className='max-w-md text-sm md:text-base text-zinc-500'>Insightful is an AI-powered sales optimization tool that provides data-driven insights to boost sales performance.</p>
