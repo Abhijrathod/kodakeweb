4bb745757996f4ce71ec1ecf9a976fb3bab5b063
Abhijeet anil Rathod <nueonboy@gmail.com>
2024-02-04 01:09:21 +0530

    "Start Scaling Today with Cloud Power!"

--- app/page.tsx
+++ app/page.tsx
@@ -16,9 +16,9 @@ export default function Home() {
         <div className='w-full h-2/4 md:h-full md:w-2/5 flex flex-col justify-center items-center md:items-start gap-8'>
           <div className='flex flex-col gap-2'>
             <h1 className='text-4xl font-black md:text-8xl'>KODAKEY</h1>
-            <h2 className='text-md md:text-2xl'>Start growing today!</h2>
+            <h2 className='text-md md:text-2xl'>Start Scaling with Cloud!"</h2>
           </div>
-          <p className='max-w-md text-sm md:text-base text-zinc-500'>Insightful is an AI-powered sales optimization tool that provides data-driven insights to boost sales performance.</p>
+          <p className='max-w-md text-sm md:text-base text-zinc-500'>Unleashing Compute Power, Robust Database Storage, Seamless Content Delivery, a Flexibility, Scalability, and Reliability.</p>
           <div className='w-full flex items-center justify-center md:justify-start gap-4'>
             <button className='w-48 h-12 text-sm sm:text-base rounded bg-white text-black hover:bg-fuchsia-700 hover:text-white transition-colors'>Try 7 days free!</button>
             <button className='w-48 h-12 text-sm sm:text-base rounded hover:bg-white hover:text-white hover:bg-opacity-5 transition-colors'>Contact</button>
